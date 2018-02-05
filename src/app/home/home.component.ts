682b5c1413e008362f538b6e7e992e08bccb25c4
NehaChaudary <35862142+NehaChaudary@users.noreply.github.com>
2018-02-05 13:27:01 +0530

    promise/observer/dav university/mca

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+myFirstSubscription : Subscription;
   constructor() { }
 
   ngOnInit() {
+    /*let myFirstObserver:Observable<number>=Observable.interval(1000);
+    this.myFirstSubscription = myFirstObserver.subscribe(
+      (data:number)=>{
+        console.log("your normal data is data",data)
+      },
+      (error)=>{
+        console.log("error is error")
+      },
+      ()=>{
+        console.log("obserevtable is done")
+      }
+    )
+    myFirstObserver.subscribe(
+      (data:number)=>{
+        console.log("your normal data is",data)
+      },
+      ()=>{
+        console.log("observable is done")
+      }
+    )*/
   }
 
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserModel } from '../model/user.model';
 import { AuthServiceImpl } from '../auth.service';
+import { resolve } from 'dns';
+import { reject } from 'q';
+
 
 
 @Component({
@@ -23,7 +26,35 @@ export class RegistrationComponent implements OnInit {
     console.log("password is ",password)
     this.authService.userSignUp(email, password)
   }
-    
+private validateCredential(email,password)
+{
+  this.validate(email,password).then((response)=>{
+    console.log(response)
+    this.signUpWithEmailAndPassword(email,password)
+  }).catch(function(error){
+    console.log(error.message)
+
+  }
+  )
+  console.log("firebase finished")
+
+}
+
+
+
+    private validate(email,password):Promise<any>{
+      console.log("inside the validate")
+      return new Promise<any>((resolve,reject)=>{
+        console.log("inside call back")
+        if(email="neha@neha.com"){
+          reject("emailis already availabe in db")
+        }
+        else{
+          resolve("user can perform login operation")
+        }
+        
+      })   
+    }
   signInWithGoogle(){
       
   }
